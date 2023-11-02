0a8adc189990b424b7b61c6a3b638b2fdce50c65
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-11-01 21:10:27 -0700

    solved # 1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 1 below
+def count(num):
+    if num == 0:
+        print(num)
+        return
+    else:
+        print(num) 
+        (count(num-1))
+
+n = 7
+count(n)
--- solution2.py
+++ solution2.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 2 below
+def natural_numbers(x,i=1):
+	if x > i:
+		return 
+	else:
+		print(i)
+		natural_numbers(x,i+1)
+natural_numbers(3)
+#output: 1 2 3
